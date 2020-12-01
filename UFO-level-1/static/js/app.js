6b1db30a5185d93143b931423e72ba59e12882db
Shona Critch <shona.critch@gmail.com>
2020-12-01 20:21:03 +1100

    added app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,35 +1,28 @@
-// from data.js
+// set data as tableData variable 
 var tableData = data;
-// select table in html file!
-var body = d3.select("tbody");
-// using Object.entries to get key, value data inside of the table
-// and loop through them to add to the table in html
-function displayData(data){ 
-    tbody.text("")
-    data.forEach(function(sighting){
-    new_tr = tbody.append("tr")
-    Object.entries(sighting).forEach(function([key, value]){
-        new_td = new_tr.append("td").text(value)	
+// add ufo sightings data to table
+function appendTable(data) {
+    d3.select("tbody").html("");
+    data.forEach((sighting) => {
+        var tabRow = d3.select("tbody").append("tr");
+        Object.values(sighting).forEach((value) => {
+            var tableData = tabRow.append("td");
+            tableData.text(value);
+        });
     })
-})}
-
-displayData(tableData)
-
-//select the web user's input and the filter button
-var dateInputText = d3.select("#datetime")
-var button = d3.select("filter-btn")
-
-// filter data with date that the user inputs
-function clickSelect(){
-    //don't refresh the page!
-    d3.event.preventDefault();
-    //print the value that was input
-    console.log(dateInputText.property("value"));
-    //create a new table showing only the filterd data
-    var new_table = tableData.filter(sighting => sighting.datetime===dateInputText.property("value"))
-    //display the new table
-    displayData(new_table);
 }
 
-// event listener to handle change and click
-dateInputText.on("change", clickSelect)
+appendTable(tableData);
+
+// create event listener and prevent default
+function clickEvent() {
+    d3.event.preventDefault();
+    var date = d3.select("#datetime").property("value");
+    var filterDateTime = tableData;
+    if (date) {
+        filterDateTime = filterDateTime.filter((row) => row.datetime === date);
+    }
+    appendTable(filterDateTime);
+}
+
+d3.selectAll("#filter-btn").on("click", clickEvent);
